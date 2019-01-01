2e5aeab72ca627eb0d373bf006138327efc37d1d
Jorge Maia <jsm.java@gmail.com>
2019-01-01 17:02:14 -0300

    Projeto Spring Data Curso Udemy Klay
    Segue o projeto

---
+++ src/main/java/com/klayrocha/firstspring/boot/FirstSpringData2Application.java
@@ -0,0 +1,14 @@
+package com.klayrocha.firstspring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringData2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringData2Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/klayrocha/firstspring/boot/FirstSpringData2ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.klayrocha.firstspring.boot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstSpringData2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
